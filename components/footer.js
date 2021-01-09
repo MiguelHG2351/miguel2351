619a010cc7fd5960b4067b328bdf22bc979bdb3c
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2021-01-09 08:20:50 -0600

    elements do not have a title

--- components/footer.js
+++ components/footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
             <footer className="footer">
                 <h2>Gracias por Visitar!!</h2>
                 <div className="iframe-container">
-                    <iframe src="https://github-readme-stats.vercel.app/api?username=Miguelhg2351&show_icons=true&hide_border=true&title_color=fff&icon_color=ffffff&bg_color=09f&text_color=fff" frameBorder="0"></iframe>
+                    <iframe title="status profile" src="https://github-readme-stats.vercel.app/api?username=Miguelhg2351&show_icons=true&hide_border=true&title_color=fff&icon_color=ffffff&bg_color=09f&text_color=fff" frameBorder="0"></iframe>
                 </div>
                 <div className="footer-info">
                     <ul className="section-footer-container">
