2417a5fc8665f045823b0b009ed42878099316cb
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2021-03-13 21:46:55 -0600

    header

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
-module.exports = {
+const withPWA = require('next-pwa')
+const runtimeCache = require('next-pwa/cache')
+
+const pwa = withPWA({
+    pwa: {
+        dest: 'public',
+        runtimeCache
+    },
+    redirects: async () => {
+        return [
+            {
+                source: '/about',
+                destination: '/',
+                permanent: true
+            }
+        ]
+    }
+})
+
+const settings = {
     redirects: async () => {
         return [
             {
@@ -9,3 +28,5 @@ module.exports = {
         ]
     }
 }
+
+module.exports = process.env.PORT ? pwa : settings
