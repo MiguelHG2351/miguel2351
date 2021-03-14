dca371b4787aa4d4ca2aa19f6dff00a8c833a957
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2021-03-14 10:45:01 -0600

    manifest | pwa

--- pages/_app.js
+++ pages/_app.js
@@ -15,6 +15,7 @@ function MyApp({Component, pageProps}) {
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
                 <meta name="google-site-verification" content="DoX9ksUU8nNRym8pev2b3JZ-BiHRVeyg2Omr8vJ0veM" />
+                <link rel="manifest" href="/manifest.json"/>
             </Head>
             <Provider store={store}>
                 <Header />
